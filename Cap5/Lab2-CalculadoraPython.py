8e74eb98cef9736d3fa9548af1cc7445200b721a
Ryu Higashi <ryuhigashi@hotmail.com>
2025-08-24 21:28:33 -0300

    starting calc lab02

---
+++ Cap5/Lab2-CalculadoraPython.py
@@ -0,0 +1,22 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capitulos até aqui no curso.
+
+print("\n**************** Calculadora em Python ****************")
+
+calc = int(input("\n Selecione o número da operação desejada:\n" \
+"1 - Soma\n" \
+"2 - Subtração\n" \
+"3 - Multiplicação\n" \
+"4 - Divisão\n" \
+"\n" \
+"Digite sua opção (1/2/3/4): "))
+
+def numFloat(num1, num2):
+    num1 = input
+
+def calculator(calc):
+    if calc == 1:
+    
+
+calculator(calc)
